f09c3aecdd3edd58429a4f652387e6258e7f94de
Drijust <justdebout509@gamil.com>
2024-10-12 08:46:42 -0300

    first commit

---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div ` ` 
+  
+  export const TopBackground = styled.div` 
+  background-color: #181f36;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-Content: space-evenly;
+  padding: 20px;
+  height: 100vh;
+`  
+
+export const TopBackground = styled.div'
+
+` 
+
+  export const Form = styled.form ` `
+
+   export const ContainerInputs = styled.div ` `
+
+    export const Input = styled.input ` `
+
+     export const InputLabel = styled.label ` `
+
+  export const Button = styled.button ` `
+
+  export const Title = styled.h2` `
